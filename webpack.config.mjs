8404eabb4ee42755a259af6d9ed393884f47b0c2
Maciej Czarnecki <mcczarny@gmail.com>
2025-08-21 14:16:35 +0200

    Make package commonJS for now

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -20,16 +20,10 @@ const extensionConfig = {
     // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
     path: path.resolve(__dirname, 'dist'),
     filename: 'extension.js',
-    library: {
-      type: 'module'
-    }
+    libraryTarget: 'commonjs2'
   },
-  experiments: {
-    outputModule: true,
-  },
-
   externals: {
-    vscode: 'module vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
     // modules added here also need to be added in the .vscodeignore file
   },
   resolve: {
